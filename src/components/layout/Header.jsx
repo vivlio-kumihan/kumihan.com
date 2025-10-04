7f970c03d8f28ec11769a74265688cd66fff0bd3
Nobuyuki Takahiro <vivlio@kumihan.com>
2025-10-05 07:40:30 +0900

    modify title

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-        <h1 className={styles.logo}>My Site</h1>
+        <h1 className={styles.logo}>Studio Quad9</h1>
         <nav className={styles.nav}>
           <a href="/">Home</a>
           <a href="/about">About</a>
