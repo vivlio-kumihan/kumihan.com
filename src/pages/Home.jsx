cb2b910fd0f2844dd047f0718db6386dcbb55e2e
Nobuyuki Takahiro <vivlio@kumihan.com>
2025-10-05 19:22:36 +0900

    fix home style, use module

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import { react } from "react";
 import MySwiper from "../components/ui/MySwiper";
+import styles from "./Home.module.scss";
 
 const Home = () => {
   return (
     <>
       <div className="container">
-        <h1>Hello React!</h1>
-        <h2>こんにちは、リアクト！</h2>
+        <div className="wrapper">
+          <h1>
+            <span>You say Good Luck, I say Hello...</span>
+            Studio Quad9
+          </h1>
+          <div className={`catch-base ${styles.catch}`}>こんにちは、スタジオ・クアッド9のWEBサイトへようこそ。</div>
+        </div>
         <MySwiper />
       </div>
     </>
